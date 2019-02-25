7f75caa45516486d423aab630a79142627161416
Christos312 <christos312@gmail.com>
2019-02-26 00:21:22 +0200

    Added first api

--- 9-forkify/starter/src/js/index.js
+++ 9-forkify/starter/src/js/index.js
@@ -1,1 +1,11 @@
-import { add, multiple } from './views/searchView';
+// 14c4ee4509a4ac69e3ede12db237082b
+// https://www.food2fork.com/api/search
+
+import axios from 'axios';
+
+async function getResults(query) {
+  const key = '14c4ee4509a4ac69e3ede12db237082b';
+  const res = await axios(`https://cors-anywhere.herokuapp.com/https://www.food2fork.com/api/search?key=${key}&q=${query}`);
+}
+
+getResults('pizza');
